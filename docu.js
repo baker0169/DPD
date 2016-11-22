5c17039f722a3fe29a6f59749901386317d4758c
EvanYo25 <b04705023@ntu.edu.tw>
2016-11-22 14:20:50 +0800

    dd

--- docu.js
+++ docu.js
@@ -1,5 +1,5 @@
 function funtionNameHere()
 {
-	var svgContainer = d3.select(".className").append("svg").attr("width", 300).attr("height", 300);
+	var svgContainer = d3.select("body").append("svg").attr("width", 300).attr("height", 300);
 	var poly = svgContainer.append("polygon").attr("points", "10,14 20,125 100,10").style("fill", "red");
 }
